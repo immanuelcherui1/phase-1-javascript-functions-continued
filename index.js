1e692f17dfde10236b44cbd23ec49efe625550bd
immanuelcherui1 <mmanuelcherui1@gmail.com>
2023-12-15 03:31:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(satFun = "roller-skate"){
+    return `This Saturday, I want to ${satFun}!`
+
+}console.log(saturdayFun());
+
+function mondayWork(satFun = "go to the office"){
+    return `This Monday, I will ${satFun}.`
+
+}console.log(mondayWork());
+
+function wrapAdjective(wrapper = "*"){
+    return function (adjective = "a hard worker"){
+    return `You are ${wrapper}${adjective}${wrapper}!`}
+
+}
